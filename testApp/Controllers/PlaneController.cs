9a5985a3eddbf0c658542cf8ca534a6f3a317666
Casselman jasper <jasper.casselman@student.howest.be>
2018-04-21 16:37:39 +0200

    changes by me

---
+++ testApp/Controllers/PlaneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testApp.Controllers
+{
+    [Route("")]
+    public class PlaneController : Controller
+    {
+        [Route("")] // Index is de default route
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[controller]/[action]")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
